ec8ac31301aa49f26bdd52ddbd7bfebeea54abce
Arnaud POINTET <arnaud.pointet@gmail.com>
2018-03-16 14:59:43 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var $buoop = {notify:{e:-6,f:-6,o:-6,s:-1,c:-6},insecure:true,unsupported:true,api:5}; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
